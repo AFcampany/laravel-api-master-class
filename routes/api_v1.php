ddbc9a6f55fea7fe00b2c034631491ba09772363
Younes Anis <eldjoudi.younes.anis@gmail.com>
2024-11-26 11:23:01 +0100

    ep5: implemant login, use Sanctum\HasApiToken trait, auth:sanctum meddleware, use createToken and plainTextToken

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::apiResource('/tickets', TicketController::class);
+Route::middleware('auth:sanctum')
+    ->apiResource('/tickets', TicketController::class);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
