42515a636c09f48355a22d56c6d7e80a15f8ef63
Younes Anis <eldjoudi.younes.anis@gmail.com>
2024-11-26 10:47:26 +0100

    ep 4: api versioning and register api_v1.php file in bootstrap/app.php

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,17 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::get('/', function() {
+    return response()->json([
+        'message' => "web server runing",
+        'status' => 200,
+    ], 200);
+});
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/tickets', function() {
-    return Ticket::all();
-});
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
