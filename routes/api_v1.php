312209ce18b64a76c4cbd07ae69703c8b1923fa0
Younes Anis <eldjoudi.younes.anis@gmail.com>
2024-11-26 15:07:37 +0100

    ep8: add a user: controler, request, resource. add rleationship of ->user. use mergeWhen inside resource for data witsame condition and get the user data from the ticket route via reltion

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::middleware('auth:sanctum')
     ->apiResource('/tickets', TicketController::class);
+
+Route::middleware('auth:sanctum')
+    ->apiResource('/users', UserController::class);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
